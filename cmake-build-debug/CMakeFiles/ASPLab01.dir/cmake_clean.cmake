2408d0cbf546f2a835ce220bb04cc0291b9c1701
MaxiAgrippa <maxiagrippamail@mail.com>
2020-05-26 01:59:17 -0400

    v0.01

---
+++ cmake-build-debug/CMakeFiles/ASPLab01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "ASPLab01"
+  "ASPLab01.pdb"
+  "CMakeFiles/ASPLab01.dir/src/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ASPLab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
